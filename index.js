dd29d1058f46117de416c0007b168f76a7da925b
minternational <mintkuehn@gmail.com>
2021-04-13 17:14:39 +0200

    add some design with css and align the content

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ fetch(url)
   .then(data => {
     data.forEach(coin => {
       coinArray.push(coin);
+      console.log(coin);
     });
   });
 
-const getInput = () => {
-  console.log(document.getElementById("inputValue").value);
-};
+const input = document.getElementById("inputValue");
+
+input.addEventListener("keyup", e => {
+  if (e.keyCode === 13) {
+    document.getElementById("searchCoin").click();
+  }
+});
 
 // document.getElementById("getCoins").addEventListener("click", getCoins);
 
 const searchCoin = () => {
   // const searchQuery = prompt("Which coin do you want to see?");
-
   const searchQuery = document.getElementById("inputValue").value;
-
   const lowerCaseSearchQuery = searchQuery.toLowerCase();
-  const filtered = coinArray.filter(coin => coin.id === lowerCaseSearchQuery);
-  console.log(filtered[0].id);
+
+  const filtered = coinArray.filter(coin => lowerCaseSearchQuery === coin.id);
 
   let createImg = document.createElement("img");
   createImg.setAttribute("src", `${filtered[0].image}`);
-  createImg.setAttribute("style", "width: 50px");
-
+  createImg.setAttribute("style", "width: 75px");
   document.body.appendChild(createImg);
 
   let createH1 = document.createElement("h1");
   createH1.innerHTML = `${filtered[0].id} <br>`;
-
   document.body.appendChild(createH1);
 
   let createP = document.createElement("p");
-  createP.innerHTML = `Current price: ${filtered[0].current_price} USD `;
-
+  createP.innerHTML = `<b>Symbol:</b> ${filtered[0].symbol} <br> <b>Current price:</b> ${filtered[0].current_price} USD <br> <b>Price change in last 24h:</b> ${filtered[0].price_change_percentage_24h}%`;
   document.body.appendChild(createP);
 
   document.getElementById("inputValue").value = "";
